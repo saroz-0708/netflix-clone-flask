f36a14af0ce44ca6b0b68c1919e0d0239e7ceaf2
Saro <saro.codes@gmail.com>
2026-03-01 12:32:39 +0530

    Use Railway PORT environment variable

--- app.py
+++ app.py
@@ -316,5 +316,8 @@ def add_rating(movie_id):
 
 
 # ---------- RUN ----------
+import os
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
